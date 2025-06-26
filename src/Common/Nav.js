7f54e4dba7a3324cba53dc94a8e615ab90c60c21
Adrian Dinu <12354033+adrd@users.noreply.github.com>
2025-06-26 14:46:05 +0300

    Lesson 9 - Active link states with <NavLink />.

--- src/Common/Nav.js
+++ src/Common/Nav.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { css } from "@emotion/css";
 
 const NavStyles = css`
@@ -9,14 +9,17 @@ const NavStyles = css`
     padding: 6px 12px;
     text-decoration: none;
     border-radius: 4px;
+    &.active {
+      color: #50fa7b;
+      border: 2px solid #50fa7b;
+    }
   }
 `;
 
-Link;
 const Nav = () => (
   <nav className={NavStyles}>
-    <Link to="/">Products</Link>
-    <Link to="/admin">Admin</Link>
+    <NavLink to="/">Products</NavLink>
+    <NavLink to="/admin">Admin</NavLink>
   </nav>
 );
 
