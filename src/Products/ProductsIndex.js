534cc2110879baec978d8856dc2dc58ccbfad54e
Adrian Dinu <12354033+adrd@users.noreply.github.com>
2025-06-26 16:10:15 +0300

    Lesson 12 - Fetching Data in Nested Routes.

--- src/Products/ProductsIndex.js
+++ src/Products/ProductsIndex.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { listProducts } from "./ProductsService";
 
 const ProductsIndex = () => {
-  return <div>List the products...</div>;
+  console.log("ProductsIndex start executing...");
+  const [products, setProducts] = useState(null);
+
+  useEffect(() => {
+    console.log("ProductsIndex useEffect start executing...");
+
+    (async () => {
+      console.log("ProductsIndex listProducts() start executing...");
+      const data = await listProducts();
+
+      console.log(data);
+
+      setProducts(data);
+    })();
+  }, []);
+
+  if (products === null) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      {products.map((item) => (
+        <div key={item.id}>{item.name}</div>
+      ))}
+    </div>
+  );
 };
 
 export default ProductsIndex;
