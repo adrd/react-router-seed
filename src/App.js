4c2ca031eb4e283a1b1ded72101edece5c15df75
Adrian Dinu <12354033+adrd@users.noreply.github.com>
2025-06-26 13:13:24 +0300

    Lesson 5 - Switching <Route /> elements with <Routes />.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { css } from "@emotion/css";
+
+import Products from "./Products/Products";
+import Admin from "./Admin/Admin";
+
+const AppStyles = css`
+  margin: 50px auto;
+  width: 380px;
+  .Container {
+    background: #1d1e26;
+    border: 4px solid #9580ff;
+    border-radius: 6px;
+    padding: 25px;
+  }
+`;
 
 const App = () => {
   return (
-    <div>
+    <div className={AppStyles}>
       <Router>
-        <Routes>
-          <Route path="/" element={<div>Root</div>} />
-          <Route path="/123" element={<div>123</div>} />
-        </Routes>
+        <div className="Container">
+          <Routes>
+            <Route path="/" element={<Products />} />
+            <Route path="/admin" element={<Admin />} />
+          </Routes>
+        </div>
       </Router>
     </div>
   );
---
+++ src/Products/Products.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { css } from "@emotion/css";
+
+const ProductStyles = css`
+  display: flex;
+  flex-direction: column;
+  .Logo {
+    width: 125px;
+    margin: 0 auto 25px;
+  }
+`;
+
+const Products = () => (
+  <div className={ProductStyles}>
+    <img src="/assets/img/logo.svg" alt="Ultimate Burgers" />
+    Products!
+  </div>
+);
+
+export default Products;
