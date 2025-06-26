68cafc8f6f30a973820b5f468453c2aac55614ae
Adrian Dinu <12354033+adrd@users.noreply.github.com>
2025-06-26 12:30:52 +0300

    Lesson 4 - <Route /> and Path declarations.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
-      <BrowserRouter>Hello React</BrowserRouter>
+      <Router>
+        <Routes>
+          <Route path="/" element={<div>Root</div>} />
+          <Route path="/123" element={<div>123</div>} />
+        </Routes>
+      </Router>
     </div>
   );
 };
