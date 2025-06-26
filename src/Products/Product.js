be0d07ac83b27da1ef4b5e27a9c4d25a4d59920b
Adrian Dinu <12354033+adrd@users.noreply.github.com>
2025-06-26 19:51:55 +0300

    Lesson 15 - Reading Route Params with useParams() Hook.

--- src/Products/Product.js
+++ src/Products/Product.js
@@ -1,6 +1,14 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const Product = () => {
+  console.log("Product component start executing...");
+  // const params = useParams();
+  // console.log(params);
+  const { id } = useParams();
+
+  console.log(id);
+
   return <div>Product</div>;
 };
 
