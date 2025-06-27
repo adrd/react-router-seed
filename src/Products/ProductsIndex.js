3296351031fcf70382136be8869aa0f726929b75
Adrian Dinu <12354033+adrd@users.noreply.github.com>
2025-06-27 19:30:00 +0300

    Lesson 31 - Reading Search Params with useSearchParams().

--- src/Products/ProductsIndex.js
+++ src/Products/ProductsIndex.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useSearchParams } from "react-router-dom";
 
 import ProductCard from "./ProductCard";
 
@@ -8,13 +8,24 @@ import { listProducts } from "./ProductsService";
 const ProductsIndex = () => {
   console.log("ProductsIndex start executing...");
 
-  const location = useLocation();
-  console.log(location);
+  // const location = useLocation();
+  // console.log(location);
+
+  const { state } = useLocation();
+  console.log(state);
 
   const [products, setProducts] = useState(null);
+  const [searchParams] = useSearchParams();
+
+  useEffect(() => {
+    if (state) {
+      console.warn(`Nothing found for ${state.id}`);
+    }
+  }, []);
 
   useEffect(() => {
     console.log("ProductsIndex useEffect start executing...");
+    console.log(Object.fromEntries([...searchParams]));
 
     (async () => {
       console.log("ProductsIndex listProducts() start executing...");
