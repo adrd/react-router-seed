46dae460d82d123efa839e008ee93e3a0220fc26
Adrian Dinu <12354033+adrd@users.noreply.github.com>
2025-06-30 14:44:35 +0300

    Lesson 36 - Code Splitting and Lazy-Loading Components.

--- src/Products/Products.js
+++ src/Products/Products.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { lazy } from "react";
 // import { Outlet } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
 import { css } from "@emotion/css";
 
-import Product from "./Product";
-import ProductsIndex from "./ProductsIndex";
+// import Product from "./Product";
+// import ProductsIndex from "./ProductsIndex";
+
+const Product = lazy(() => import("./Product"));
+const ProductsIndex = lazy(() => import("./ProductsIndex"));
 
 const ProductStyles = css`
   display: flex;
